4f8d6175c24951cbc1db35b6f923773ab8d9db55
VladislavPeshin <ruvladislav97@gmail.com>
2020-10-31 01:51:33 +0500

    Задание №2: тренировка использования  функций, классов, объектов и методов

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/RunClass.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class RunClass {
+
+  public static void main(String[] args) {
+    Point p1 = new Point(5,5);
+    Point p2 = new Point(25,25);
+    System.out.println(p1.distance(p2));
+    System.out.println(p2.distance(p1));
+  }
+
+}
