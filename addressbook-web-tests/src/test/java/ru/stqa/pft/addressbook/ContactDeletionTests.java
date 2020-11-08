51881abb07ff8a2ef5362566928189a5fe6a78de
VladislavPeshin <ruvladislav97@gmail.com>
2020-11-08 18:13:24 +0500

    Рефакторинг, резервное копирование

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactDeletionTests.java
@@ -15,7 +15,7 @@ public class ContactDeletionTests {
   }
 
   @Test
-  public void testContactDeletionTests() throws Exception {
+  public void testContactDeletion() throws Exception {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -15,7 +15,7 @@ public class GroupDeletionTests {
   }
 
   @Test
-  public void testGroupDeletionTests() throws Exception {
+  public void testGroupDeletion() throws Exception {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
