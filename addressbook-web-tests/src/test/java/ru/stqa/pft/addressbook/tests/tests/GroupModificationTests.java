471c64753a45efb7a2ba668e746b0f8640de800b
VladislavPeshin <ruvladislav97@gmail.com>
2020-11-09 00:18:50 +0500

    попытка исправить

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/tests/GroupModificationTests.java
@@ -7,13 +7,11 @@ public class GroupModificationTests extends TestBase{
 
   @Test
   public void testGroupModification() {
-    gotoGroupPage();
+    app.getNavigationHelper().gotoGroupPage("groups");
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-  }
-  public void gotoGroupPage() {
   }
 }
