5291fdac5b000cd75130c0dc36e3f84f63ee8381
VladislavPeshin <ruvladislav97@gmail.com>
2021-01-03 22:05:31 +0500

    Реализовано возможность удалённого запуска браузера

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/appmanager/ApplicationManager.java
@@ -37,7 +37,9 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
     dbHelper = new DbHelper();
+    
     if ("".equals(properties.getProperty("selenium.server"))) {
       if (browser.equals(BrowserType.FIREFOX)) {
         wd = new FirefoxDriver();
