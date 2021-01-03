6c25ee5a3dd8fb05743e16acd9f74a7d5bfa9a4b
VladislavPeshin <ruvladislav97@gmail.com>
2021-01-03 14:37:20 +0500

    Лекция 9.1 и 9.2

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.soap;
+
+import com.lavasoft.GeoIPService;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp{
+    String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("37.112.8.191");
+    Assert.assertEquals(ipLocation,"<GeoIP><Country>RU</Country><State>48</State></GeoIP>");
+  }
+}
